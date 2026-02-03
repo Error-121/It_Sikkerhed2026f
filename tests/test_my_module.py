589c5125ddaf8a9e553c4c63dd73f9e22e1ec0af
Error-121 <Frederiklandgraf@gmail.com>
2026-02-03 14:08:49 +0100

    har fjernet skip fra fejl og crash test

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -4,7 +4,6 @@ def test_pass():
     # Denne test vil passere
     assert 1 + 1 == 2
 
-@pytest.mark.skip(reason="Springes over med vilje")
 def test_fail():
     # Denne test vil fejle
     assert 1 * 1 == 3
@@ -15,7 +14,6 @@ def test_skip():
     assert False # failed test bliver ignoreret
     raise RuntimeError("Test crashede med vilje") # crash bliver også ignoreret
 
-@pytest.mark.skip(reason="Springes over med vilje")
 def test_crash():
     # Denne test crasher med en exception
     raise RuntimeError("Test crashede med vilje")
